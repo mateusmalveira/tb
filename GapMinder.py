e882e0af53db489e2f31f26fc733a65595476798
rafael <rafaeljosev10@gmail.com>
2016-01-23 13:04:59 -0300

    Definição Classes Protótipo

--- GapMinder.py
+++ GapMinder.py
@@ -4,10 +4,20 @@ Created on Fri Jan 22 04:28:50 2016
 
 @author: rafael
 """
-import numpy as np
 import matplotlib.pyplot as plt
 
+class GrapMinder(object):
+    pass
 
+class Tratamento(object):
+    pass
+
+class Plotar(object):
+    pass
+
+
+
+"""
 N = 50
 x = np.random.rand(N)
 y = np.random.rand(N)
@@ -16,3 +26,4 @@ area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radiuses
 
 plt.scatter(x, y, s=area, c=colors, alpha=0.5)
 plt.show()
+"""
