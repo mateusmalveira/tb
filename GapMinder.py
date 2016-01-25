6a1ee1e8f4aeefd371b2d557256bdded5013da1f
rafael <rafaeljosev10@gmail.com>
2016-01-25 13:56:35 -0300

    Coreção na identação e Adição da classe Plotar

--- GapMinder.py
+++ GapMinder.py
@@ -12,56 +12,54 @@ class Inicio(object):
 
     #classe inicial
     def __init__(self):# esse parenteses e o construtor
-     	__lista1 = []
-     	__lista2 = []
-     	#transforma cada linha em uma string e salva tudo em uma lista_1 e Lista_2
-    	with open ( 'indicator hiv estimated prevalence% 15-49.csv' , 'rb' ) as csvfile :
-    		spamreader = csv . reader ( csvfile , delimiter = ' ' , quotechar = '|' )
-    		for row in spamreader :
-    			__lista1.append(', ' . join ( row ))
-
-    	with open ( 'cell phone total.csv' , 'rb' ) as csvfile_2 :
-    		spamreader_1 = csv . reader ( csvfile_2 , delimiter = ' ' , quotechar = '|' )
-    		for I in spamreader_1 :
-    			 __lista2.append(', ' . join ( I ))
-		Tratamento(__lista1,__lista2)
+        __lista1 = []
+        __lista2 = []
+        #transforma cada linha em uma string e salva tudo em uma lista_1 e Lista_2
+        with open ( 'indicator hiv estimated prevalence% 15-49.csv' , 'rb' ) as csvfile :
+            spamreader = csv . reader ( csvfile , delimiter = ' ' , quotechar = '|' )
+            for row in spamreader :
+                __lista1.append(', ' . join ( row ))
+        with open ( 'cell phone total.csv' , 'rb' ) as csvfile_2 :
+            spamreader_1 = csv . reader ( csvfile_2 , delimiter = ' ' , quotechar = '|' )
+            for I in spamreader_1 :
+                __lista2.append(', ' . join ( I ))
+        Tratamento(__lista1,__lista2)
 
 
 
 #tratamento dos dados
 class Tratamento(object):
-		def __init__(self,a,b):
-			Anos_0 = []
-			Anos_1 = []
-			Pais_ST_0 = []
-			Pais_ST_1 = []
-			#fatia a primeira camada Titulo e Anos
-			for i in range(len(a)):
-				Tratamento_1 = []
-				if i == 0 :
-					Anos_0.append(a[i].split(','))
-				if i != 0 :
-					Tratamento_1.append(a[i])
-					for i in range(len(Tratamento_1)):
-						Pais_ST_0.append(Tratamento_1[i].split(','))
+	def __init__(self,a,b):
+		Anos_0 = []
+		Anos_1 = []
+		Pais_ST_0 = []
+		Pais_ST_1 = []
+		#fatia a primeira camada Titulo e Anos
+		for i in range(len(a)):
+			Tratamento_1 = []
+			if i == 0 :
+				Anos_0.append(a[i].split(','))
+			if i != 0 :
+				Tratamento_1.append(a[i])
+				for i in range(len(Tratamento_1)):
+					Pais_ST_0.append(Tratamento_1[i].split(','))
 
-			for i in range(len(b)):
-				Tratamento = []
-				if i == 0 :
-					Anos_1.append(b[i].split(','))
-				if i != 0 :
-					Tratamento.append(b[i])
-					for i in range(len(Tratamento)):
-						Pais_ST_1.append(Tratamento[i].split(','))
+		for i in range(len(b)):
+			Tratamento = []
+			if i == 0 :
+				Anos_1.append(b[i].split(','))
+			if i != 0 :
+				Tratamento.append(b[i])
+				for i in range(len(Tratamento)):
+					Pais_ST_1.append(Tratamento[i].split(','))
 
-			Cort = Corte()
-			titulo,anos = Cort.titulo(Anos_0)
-			titulo1,anos1 = Cort.titulo(Anos_1)
-			paises,St = Cort.Pais(Pais_ST_0)
-			paises1,St1 = Cort.Pais(Pais_ST_1)
-			print anos
-			print St[1]
-
+		Cort = Corte()
+		titulo,anos = Cort.titulo(Anos_0)
+		titulo1,anos1 = Cort.titulo(Anos_1)
+		paises,St = Cort.Pais(Pais_ST_0)
+		paises1,St1 = Cort.Pais(Pais_ST_1)
+		print(paises)
+        
 
 
 class Corte():
@@ -76,6 +74,9 @@ class Corte():
             Paises.append(z[I].pop(0))
         return Paises,z
 
+class Plotar():
+    def __int_(T,A,P,S):
+        pass
 
 
 Inicio()
